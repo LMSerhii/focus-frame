e2a2435bf6b1ce064b0bcf6cbd74a1cc012df903
Yevhen-Illiushko <139242145+Yevhen-Illiushko@users.noreply.github.com>
2023-09-10 16:03:27 +0300

    все переробив з нуля, але не працює кнопка

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,2 +1,30 @@
 console.log('hello');
 console.log('world');
+(() => {
+    const mobileMenu = document.querySelector('.js-menu-container');
+    const openMenuBtn = document.querySelector('.js-open-menu');
+    const closeMenuBtn = document.querySelector('.js-close-menu');
+  
+    const burgerMenu = () => {
+      const isMenuOpen =
+        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenu.classList.toggle('is-open');
+  
+      const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+    };
+  
+    openMenuBtn.addEventListener('click', burgerMenu);
+    closeMenuBtn.addEventListener('click', burgerMenu);
+  
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+      if (!e.matches) return;
+      mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      bodyScrollLock.enableBodyScroll(document.body);
+    });
+  })();
